6bf7eddc67e0d927d973fa5c5db0ae141c555309
Mikołaj Graja <mikolajgraja@gmail.com>
2020-05-13 16:40:49 +0200

    bg, margins and clearing

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const txt = document.querySelector('.txt')
 const ul = document.querySelector('ul')
 let deleteBtn = document.createElement('button');
@@ -6,28 +5,32 @@ const addBtn = document.querySelector('#addBtn');
 let doneBtn = document.createElement('button');
 let i = 0;
 
-const removeLi = function(){
+const removeLi = function () {
     event.target.parentNode.remove();
 }
-const doneLi = function(){
+const doneLi = function () {
     event.target.parentNode.classList.add('done');
 }
 
-const addLi = function(){
-    const li = document.createElement('li');
-    const newLi = ul.appendChild(li);
-    li.textContent =  txt.value + '  ';
-    txt.value = "";
-    deleteBtn = document.createElement('button');
-    doneBtn = document.createElement('button');
-    deleteBtn.textContent = "Usuń";
-    doneBtn.textContent = "Gotowe"
-    li.appendChild(deleteBtn);
-    li.appendChild(doneBtn);
-    i++;
+const addLi = function () {
+    if (txt.value != "") {
+        const li = document.createElement('li');
+        const newLi = ul.appendChild(li);
+        li.textContent = txt.value + '  ';
+        txt.value = "";
+        deleteBtn = document.createElement('button');
+        doneBtn = document.createElement('button');
+        deleteBtn.textContent = "Usuń";
+        doneBtn.textContent = "Gotowe";
+        li.appendChild(deleteBtn);
+        li.appendChild(doneBtn);
+        i++;
 
-    deleteBtn.addEventListener('click', removeLi);
-    doneBtn.addEventListener('click', doneLi);
+        deleteBtn.addEventListener('click', removeLi);
+        doneBtn.addEventListener('click', doneLi);
+    } else {
+        alert("Nieprawidłowy format")
+    }
 }
 
 addBtn.addEventListener('click', addLi);
